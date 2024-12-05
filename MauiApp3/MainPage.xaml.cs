ab9eaef5412efff8911d52078e3fbe05d7e32957
Fabiandislikesgibb <162465093+Fabiandislikesgibb@users.noreply.github.com>
2024-12-05 10:42:06 +0100

    draft

--- MauiApp3/MainPage.xaml.cs
+++ MauiApp3/MainPage.xaml.cs
@@ -2,8 +2,22 @@
 
 public partial class MainPage : TabbedPage
 {
+    public DateTime SelectedDate { get; set; } = DateTime.Now;
+    public string CurrentView { get; set; } = "Month"; // Default view
+
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void OnTodayButtonClicked(object sender, EventArgs e)
+    {
+        SelectedDate = DateTime.Now;
+        // Update the view to reflect the current date
+    }
+
+    private void OnViewChanged(object sender, EventArgs e)
+    {
+        // Logic to handle view change
     }
 }
