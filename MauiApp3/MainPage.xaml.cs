40b7b2e54534a751ea381558ae621cc930744267
Fabiandislikesgibb <162465093+Fabiandislikesgibb@users.noreply.github.com>
2024-12-05 11:07:19 +0100

    first draft currently have the problem with the view that it doesent recognizes the background color

--- MauiApp3/MainPage.xaml.cs
+++ MauiApp3/MainPage.xaml.cs
@@ -8,6 +8,7 @@ public partial class MainPage : TabbedPage
     public MainPage()
     {
         InitializeComponent();
+        UpdateButtonColors();
     }
 
     private void OnTodayButtonClicked(object sender, EventArgs e)
@@ -16,8 +17,40 @@ public partial class MainPage : TabbedPage
         // Update the view to reflect the current date
     }
 
-    private void OnViewChanged(object sender, EventArgs e)
+    private void OnDayViewButtonClicked(object sender, EventArgs e)
     {
-        // Logic to handle view change
+        CurrentView = "Day";
+        UpdateButtonColors();
     }
+
+    private void OnWeekViewButtonClicked(object sender, EventArgs e)
+    {
+        CurrentView = "Week";
+        UpdateButtonColors();
+    }
+
+    private void OnMonthViewButtonClicked(object sender, EventArgs e)
+    {
+        CurrentView = "Month";
+        UpdateButtonColors();
+    }
+
+    private void OnYearViewButtonClicked(object sender, EventArgs e)
+    {
+        CurrentView = "Year";
+        UpdateButtonColors();
+    }
+
+    private void UpdateButtonColors()
+    {
+        DayViewButtonColor = CurrentView == "Day" ? "#3A75C4" : "#F2F2F2";
+        WeekViewButtonColor = CurrentView == "Week" ? "#3A75C4" : "#F2F2F2";
+        MonthViewButtonColor = CurrentView == "Month" ? "#3A75C4" : "#F2F2F2";
+        YearViewButtonColor = CurrentView == "Year" ? "#3A75C4" : "#F2F2F2";
+    }
+
+    public string DayViewButtonColor { get; set; }
+    public string WeekViewButtonColor { get; set; }
+    public string MonthViewButtonColor { get; set; }
+    public string YearViewButtonColor { get; set; }
 }
